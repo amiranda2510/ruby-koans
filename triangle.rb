6b669e88f7c8b811b943502d0bbb44d3010b573c
andromeda2510 <amiranda2510@gmail.com>
2021-05-11 10:54:44 -0500

    Done

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  unless a + b > c && a + c > b && b + c > a
+    raise TriangleError.new
+  end
+
+  if a == b && a == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  else 
+    :scalene    
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
